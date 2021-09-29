17eec5e1d35b5c0fa8d1f9ab5f4c75155f7e063a
Henry Nhan <shadowhen89@gmail.com>
2021-09-28 20:24:59 -0500

    Added email validation and validation error for user (#6)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template, url_for
+from email_validator import EmailNotValidError, validate_email
 
 app = Flask(__name__)
 
@@ -8,9 +9,16 @@ def home():
 
 @app.route("/login/", methods=["POST", "GET"])
 def login():
+    error = None
     if request.method == "POST":
-        pass
-    return render_template("login.html")
+        email = request.form["email"]
+        password = request.form["password"]
+
+        try:
+            validate_email(email)
+        except EmailNotValidError as e:
+            error = str(e)
+    return render_template("login.html", error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
